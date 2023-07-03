00d6b1963a7706f685a4b545be02dd36499921aa
Nagpal45 <nagpal4503@gmail.com>
2023-07-03 22:02:19 +0530

    ...

--- client/src/pages/donation/donation.jsx
+++ client/src/pages/donation/donation.jsx
@@ -15,7 +15,7 @@ const [donations, setDonations] = useState([]);
 useEffect(() => {
   const fetchDonations = async () => {
     const res = await axios.get('/api/donation');
-    setDonations(res.data);
+    setDonations(res.data.data);
   };
   fetchDonations();
 }, [token]);
