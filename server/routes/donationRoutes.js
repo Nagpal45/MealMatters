a24ba2fb0645699b1b9b754173954955aa41f119
Nagpal45 <nagpal4503@gmail.com>
2023-07-04 13:41:07 +0530

    .

--- server/routes/donationRoutes.js
+++ server/routes/donationRoutes.js
@@ -4,7 +4,7 @@ const authenticate = require('../authenticate');
 
 router.get('/',authenticate, async (req, res) => {
     try {
-        const donations = await Donation.find().limit(8); 
+        const donations = await Donation.find({user: req.user}); 
         res.status(200).json(donations);
     } catch (err) {
         res.status(500).json(err);
